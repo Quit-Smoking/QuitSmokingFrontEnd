fcb814cdcec2e74f739c6a0cc4ee356ff6d7584a
Sung-young <smes289@gmail.com>
2024-11-12 02:50:58 +0900

    상점 예시 data 생성

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,25 @@
-import React from "react";
 import "./Shop.css";
 
 function Shop() {
+  const sampleProducts = [
+    {
+      id: 1,
+      name: "1+1 금연스티커 패치",
+      price: "35,000원",
+      image_url: "https://via.placeholder.com/150",
+      description: "1+1 금연스티커 패치 금연 보조제 모션 스티커 30매입",
+      rating: 4.5,
+    },
+    {
+      id: 2,
+      name: "특허상품 금연 스티커",
+      price: "15,800원",
+      image_url: "https://via.placeholder.com/150",
+      description: "특허상품 금연 스티커 35p 보조제품 실제로 한달간 ...",
+      rating: 4.0,
+    },
+  ];
+
   return <div>shop</div>;
 }
 
