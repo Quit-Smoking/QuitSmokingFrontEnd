220e0a5aa096c0dfa786357fafafa1764366cb0a
Sung-young <smes289@gmail.com>
2024-11-12 02:47:19 +0900

    App.jsx 라우터 로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import Result from './pages/Result/Result';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Result from "./pages/Result/Result";
+import Shop from "./pages/Shop/Shop";
 
 function App() {
-
   return (
-    <>
-      <Result />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/Result" element={<Result />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
