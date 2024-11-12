bf884d73826368e6de2a10228922f0ccbf3359c7
Sung-young <smes289@gmail.com>
2024-11-12 22:37:52 +0900

    shop_explain_page

--- src/pages/Shop/ItemExplain.jsx
+++ src/pages/Shop/ItemExplain.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import "./ItemExplain.css";
 
 function ItemExplain({ onClose }) {
@@ -6,31 +7,34 @@ function ItemExplain({ onClose }) {
       <div className="modal-content">
         <h2>나에게 맞는 보조제는?</h2>
         <p>금연보조제는 니코틴 의존을 줄이는데 도움이 됩니다.</p>
+        <div className="borderbottom"></div>
 
         <div className="explanation-section">
-          <h3>급단현상이 심하다면</h3>
-          <p>니코틴 패치</p>
+          <p className="color-text">급단현상이 심하다면</p>
+          <h3>니코틴 패치</h3>
           <p>
             니코틴 패치는 기상 직후부터 흡연 욕구가 강하고 금단 현상이 심한
             흡연자에게 적합한 제품이며, 니코틴 패치는 지속적으로 일정한 농도의
             니코틴을 공급해준다는 특징이 있어요.
           </p>
+          <div className="borderbottom"></div>
         </div>
 
         <div className="explanation-section">
-          <h3>충동적인 흡연자라면</h3>
-          <p>니코틴 껌·사탕</p>
+          <p className="color-text">충동적인 흡연자라면</p>
+          <h3>니코틴 껌·사탕</h3>
           <p>
             흡연 욕구가 충동적으로 발생하는 흡연자라면 니코틴 껌 또는 니코틴
             사탕을 추천합니다. 이러한 제품은 즉시 효과가 빠르게 나타나며, 니코틴
             겔은 흡연 욕구가 있을 때마다 하루 최대 30분 동안 천천히 씹도록
             합니다.
           </p>
+          <div className="borderbottom"></div>
         </div>
 
         <div className="explanation-section">
-          <h3>니코틴 의존성이 낮다면</h3>
-          <p>니코틴 필름</p>
+          <p className="color-text">니코틴 의존성이 낮다면</p>
+          <h3>니코틴 필름</h3>
           <p>
             니코틴 의존성이 낮은 흡연자에게는 니코틴 구강용 해소제가 적합합니다.
             사용 시 6주간 하루 12개 이하로 사용하며 점차 줄여 나갑니다. 하루
@@ -46,5 +50,9 @@ function ItemExplain({ onClose }) {
     </div>
   );
 }
+
+ItemExplain.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
 
 export default ItemExplain;
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./Shop.css";
 import ShopExplain from "../../assets/Shop.svg";
+import CloseIcon from "../../assets/close.svg";
+import CheckIcon from "../../assets/check.svg";
 import ItemExplain from "./ItemExplain";
 
 function Shop() {
   const [isExplainOpen, setIsExplainOpen] = useState(false);
+  const [isSortModalOpen, setIsSortModalOpen] = useState(false);
+  const [sortOption, setSortOption] = useState("인기순");
 
   const sampleProducts = [
     {
@@ -44,6 +48,14 @@ function Shop() {
   const openExplainModal = () => setIsExplainOpen(true);
   const closeExplainModal = () => setIsExplainOpen(false);
 
+  const openSortModal = () => setIsSortModalOpen(true);
+  const closeSortModal = () => setIsSortModalOpen(false);
+
+  const handleSortOptionClick = (option) => {
+    setSortOption(option);
+    closeSortModal();
+  };
+
   return (
     <div className="shop-container">
       <header className="shop-header">
@@ -67,7 +79,9 @@ function Shop() {
       </div>
       <div className="itemtotal">
         <div>총 100개</div>
-        <div>인기순 ▼</div>
+        <div onClick={openSortModal} className="sort-option">
+          {sortOption} ▼
+        </div>
       </div>
       <div className="product-grid">
         {sampleProducts.map((product) => (
@@ -87,6 +101,47 @@ function Shop() {
         ))}
       </div>
       {isExplainOpen && <ItemExplain onClose={closeExplainModal} />}
+      {isSortModalOpen && (
+        <div className="sort-modal">
+          <div className="sortTitle">
+            <h3>정렬</h3>
+            <button onClick={closeSortModal} className="sort-modal-close">
+              <img src={CloseIcon} alt="close" />
+            </button>
+          </div>
+
+          <div className="sortlist">
+            <div
+              className={`list ${sortOption === "인기순" ? "selected" : ""}`}
+              onClick={() => handleSortOptionClick("인기순")}
+            >
+              인기순{" "}
+              {sortOption === "인기순" && <img src={CheckIcon} alt="check" />}
+            </div>
+            <div
+              className={`list ${sortOption === "판매량순" ? "selected" : ""}`}
+              onClick={() => handleSortOptionClick("판매량순")}
+            >
+              판매량순{" "}
+              {sortOption === "판매량순" && <img src={CheckIcon} alt="check" />}
+            </div>
+            <div
+              className={`list ${sortOption === "최신순" ? "selected" : ""}`}
+              onClick={() => handleSortOptionClick("최신순")}
+            >
+              최신순{" "}
+              {sortOption === "최신순" && <img src={CheckIcon} alt="check" />}
+            </div>
+            <div
+              className={`list ${sortOption === "평점순" ? "selected" : ""}`}
+              onClick={() => handleSortOptionClick("평점순")}
+            >
+              평점순{" "}
+              {sortOption === "평점순" && <img src={CheckIcon} alt="check" />}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
