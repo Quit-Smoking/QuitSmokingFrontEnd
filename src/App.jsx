2e2a851b2e495f4d32a7007b3c4940cee6895443
glory-yun <yyk1797@gmail.com>
2024-11-16 21:10:41 +0900

    make startpage

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Result from "./pages/Result/Result";
 import Shop from "./pages/Shop/Shop";
 import Login from "./pages/LogIn/Login";
+import StartPage from "./pages/Main/StartPage";
 
 function App() {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/Result" element={<Result />} />
-    //     <Route path="/shop" element={<Shop />} />
-    //   </Routes>
-    // </Router>
-    <Login />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/Result" element={<Result />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/startpage" element={<StartPage />} />
+        </Routes>
+      </Router>
+      <Login />
+    </>
   );
 }
 
