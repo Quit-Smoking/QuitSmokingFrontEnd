e167649169e757b77bdc048cfdd8cf18c59165f0
Your Name <alex2321218@gmail.com>
2024-11-27 22:04:37 +0900

    feat: new mission page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Result from "./pages/Result/Result";
 import Shop from "./pages/Shop/Shop";
+import NewMission from "./pages/Mission/NewMission";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/Result" element={<Result />} />
-        <Route path="/shop" element={<Shop />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/Result" element={<Result />} />
+    //     <Route path="/shop" element={<Shop />} />
+    //   </Routes>
+    // </Router>
+    <NewMission />
   );
 }
 
