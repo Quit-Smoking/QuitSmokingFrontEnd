dd6ecf24444b5b098ffdc18d8be295491b549158
Sung-young <smes289@gmail.com>
2024-11-29 19:24:25 +0900

    Singup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import Login from "./pages/LogIn/Login";
+import StartPage from "./pages/Main/StartPage";
+import Survey from "./pages/Main/Survey";
+import Home from "./pages/Main/Home";
 import Result from "./pages/Result/Result";
 import Shop from "./pages/Shop/Shop";
-
 import MissionDetail from "./pages/MissionDetail/MissionDetail";
-import Diary from "./pages/Diary/Diary";
+import Signup from "./pages/Singup/Signup";
+// import Diary from "./pages/Diary/Diary";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -18,10 +20,10 @@ function App() {
         <Route path="/startpage/survey" element={<Survey />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/missionDetail" element={<MissionDetail />} />
+        <Route path="/Signup" element={<Signup />} />
       </Routes>
-    {/* <Login /> */}
+      {/* <Login /> */}
     </>
-
   );
 }
 
