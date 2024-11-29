fcf976a52252e40f3432d3c06a6626c24b0a117d
Sung-young <smes289@gmail.com>
2024-11-29 15:10:17 +0900

    Connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
-
-import Login from "./pages/LogIn/Login";
-import StartPage from "./pages/Main/StartPage";
-import Survey from "./pages/Main/Survey";
-import Home from "./pages/Main/Home";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Result from "./pages/Result/Result";
 import Shop from "./pages/Shop/Shop";
-
 import Diary from "./pages/Diary/Diary";
-import Login from "./pages/LogIn/Login";
+// import Login from "./pages/LogIn/Login";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/Result" element={<Result />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/Diary" element={<Diary />}></Route>
-      </Routes>
-    </Router>
-    <Login />
-
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
-
-
-function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/Result" element={<Result />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/startpage" element={<StartPage />} />
-          <Route path="/startpage/survey" element={<Survey />} />
-          <Route path="/Home" element={<Home />} />
-        </Routes>
-      </Router>
-      <Login />
-    </>
+    <Routes>
+      <Route path="/Result" element={<Result />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/Diary" element={<Diary />} />
+    </Routes>
+    // <Login />
   );
 }
 
