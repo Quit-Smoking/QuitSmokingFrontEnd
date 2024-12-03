61a776aa99ed02e641fe2d2af7c45e737691056f
Your Name <alex2321218@gmail.com>
2024-12-03 13:58:23 +0900

    feat: login and mission pages api connection

--- src/pages/Mission/createMission/CreateMission.jsx
+++ src/pages/Mission/createMission/CreateMission.jsx
@@ -7,16 +7,17 @@ import StartMission from "../StartMission";
 function NewMission() {
     const navigate = useNavigate();
     const [currentPage, setCurrentPage] = useState(0); // 현재 페이지 상태 (0: page0, 1: page1, 2: page2)
-    const [missionContent, setMissionContent] = useState(""); // 입력된 텍스트 상태
+    const [missionName, setMissionName] = useState(""); // 입력된 텍스트 상태
     const [selectedPeriod, setSelectedPeriod] = useState(null); // 선택된 기간
     const [selectedDays, setSelectedDays] = useState([]); // 선택된 요일
     const periods = ["1주", "2주", "4주", "8주", "12주", "16주"]; // 선택 가능한 기간
     const weekdays = ["월", "화", "수", "목", "금"]; // 평일
     const weekends = ["토", "일"]; // 주말
+    const defaultMission = false;
 
     // 입력값 변경 핸들러
     const handleInputChange = (e) => {
-        setMissionContent(e.target.value);
+        setMissionName(e.target.value);
     };
 
     // 기간 선택 처리
@@ -42,12 +43,15 @@ function NewMission() {
 
     // 다음 버튼 핸들러
     const handleNext = () => {
-        if (currentPage === 0 && missionContent) {
+        if (currentPage === 0 && missionName) {
             setCurrentPage(1);
-        } else if (currentPage === 1 && selectedPeriod !== null) {
+        } else if (currentPage === 1 && selectedDays.length > 0) {
             setCurrentPage(2);
-        } else if (currentPage === 2 && selectedDays.length > 0) {
-            navigate("/createMission/startmission"); // startMission()으로 이동
+            console.log("이동 시도")
+            console.log(`missionName, selectedDays, defaultMisison: ${missionName}, ${selectedDays}, ${defaultMission}`)
+            navigate("/startmission", {
+                state: { missionName, selectedDays, defaultMission },
+            }); // startMission()으로 이동
         }
     };
 
@@ -68,70 +72,27 @@ function NewMission() {
                         className="mission-input"
                         type="text"
                         placeholder="내용을 입력하세요"
-                        value={missionContent}
+                        value={missionName}
                         onChange={handleInputChange}
                     />
                 </div>
             </main>
             <footer className="mission-footer">
                 <button
-                    className={`mission-next-btn-single ${missionContent ? "active" : ""
+                    className={`mission-next-btn-single ${missionName ? "active" : ""
                         }`}
                     onClick={handleNext}
-                    disabled={!missionContent}
+                    disabled={!missionName}
                 >
                     다음
                 </button>
             </footer>
         </div>,
 
-        // Page1
-        <div className="create-mission-container" key="page1">
-            <header>
-                <TopBar title="카페인 섭취 안 하기" onBack={() => setCurrentPage(0)} />
-            </header>
-            <main className="mission-main">
-                <div className="mission-text-container">
-                    <h1 className="mission-title">기간 설정</h1>
-                    <p className="mission-subtitle">미션을 진행할 기간을 설정해주세요</p>
-                </div>
-                <div className="mission-grid-container">
-                    {periods.map((period, index) => (
-                        <div
-                            key={index}
-                            className={`mission-period-box ${selectedPeriod === index ? "selected" : ""
-                                }`}
-                            onClick={() => handlePeriodSelect(index)}
-                        >
-                            {period}
-                        </div>
-                    ))}
-                </div>
-            </main>
-            <footer className="mission-footer">
-                <div className="mission-btn-pair">
-                    <button
-                        className={`mission-prev-btn active`}
-                        onClick={handlePrev}
-                    >
-                        이전
-                    </button>
-                    <button
-                        className={`mission-next-btn ${selectedPeriod !== null ? "active" : ""
-                            }`}
-                        onClick={handleNext}
-                        disabled={selectedPeriod === null}
-                    >
-                        다음
-                    </button>
-                </div>
-            </footer>
-        </div>,
-
         // Page2
         <div className="create-mission-container" key="page2">
             <header>
-                <TopBar title="카페인 섭취 안 하기" onBack={() => setCurrentPage(1)} />
+                <TopBar title={missionName} onBack={() => setCurrentPage(1)} />
             </header>
             <main className="mission-main">
                 <div className="mission-text-container">
