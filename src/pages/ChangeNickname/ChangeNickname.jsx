17a5cb16807f030441f1c817a52a490244cc9b01
Sung-young <smes289@gmail.com>
2024-12-04 20:45:45 +0900

    닉네임변경 페이지 구현

--- src/pages/ChangeNickname/ChangeNickname.jsx
+++ src/pages/ChangeNickname/ChangeNickname.jsx
@@ -1,43 +1,23 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./ResetPassword.css";
+import "./ChangeNickname.css";
 import Cancel from "../../assets/cancel.svg";
 
 function ChangeNickname() {
-  const [password, setPassword] = useState(""); // 비밀번호 필드
-  const [confirmPassword, setConfirmPassword] = useState(""); // 비밀번호 재입력 필드
-
-  const [passwordMessage, setPasswordMessage] = useState(""); // 비밀번호 메시지
-  const [isPasswordValid, setIsPasswordValid] = useState(null);
+  const [nickname, setNickname] = useState(""); // 비밀번호 필드
 
   const navigate = useNavigate();
 
   // 비밀번호 입력 처리
   const handlePasswordChange = (e) => {
     const value = e.target.value;
-    setPassword(value);
-
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    if (!passwordRegex.test(value)) {
-      setPasswordMessage(
-        "⚠️비밀번호는 영어, 숫자를 포함하여 8자리 이상이어야 합니다"
-      );
-      setIsPasswordValid(false);
-    } else {
-      setPasswordMessage("사용 가능한 비밀번호입니다");
-      setIsPasswordValid(true);
-    }
+    setNickname(value);
   };
 
   // 버튼 클릭 시 검증 및 제출
   const handleSubmit = async () => {
     // 비밀번호와 재입력 값이 다를 경우 메시지 표시
-    if (password !== confirmPassword) {
-      setConfirmPasswordMessage("⚠️비밀번호가 일치하지 않습니다");
-      setIsConfirmPasswordValid(false);
-      return; // 검증 실패 시 함수 종료
-    }
     const token =
       "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwiaWF0IjoxNzMzMzA3MzY0LCJleHAiOjE3MzMzNDMzNjR9.M3agOzCMGRrPsOukqEe-MGTKH_1nx8hOulHQxipIfjU"; // 로컬 스토리지에서 토큰 가져오기
 
@@ -45,7 +25,7 @@ function ChangeNickname() {
     const requestData = {
       //   token: localStorage.getItem("token"), // 로컬 스토리지에서 토큰 가져오기
       token: token,
-      rawPassword: password,
+      nickname: nickname,
     };
 
     try {
@@ -95,20 +75,16 @@ function ChangeNickname() {
           <input
             type="password"
             id="password"
-            placeholder="비밀번호"
-            value={password}
+            placeholder="닉네임"
+            value={nickname}
             onChange={handlePasswordChange}
           />
-          <p className={isPasswordValid ? "valid-message" : "invalid-message"}>
-            {passwordMessage}
-          </p>
         </div>
       </div>
 
       <button
         className="signup-button"
         onClick={handleSubmit} // 버튼 클릭 시 검증 및 처리
-        disabled={!isPasswordValid} // 비밀번호 검증이 완료되지 않으면 버튼 비활성화
       >
         확인
       </button>
